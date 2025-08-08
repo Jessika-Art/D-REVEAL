a4c04c70a9d8274cc929b0233c17a4888828168c
Jessika-Art <amarildo12cuka@gmail.com>
2025-08-08 22:35:24 +0100

    g

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,6 +108,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className="font-sans antialiased">
         <GoogleAnalytics />
         <StructuredData />
